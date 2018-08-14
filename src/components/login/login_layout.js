3a0f15ec76e13d9a2672ea15a828ed9b5bd34866
Karolina <karolina.chrzaszcz@euvic.pl>
2018-08-14 10:22:33 +0200

    Add grid to login_layout

--- src/components/login/login_layout.js
+++ src/components/login/login_layout.js
@@ -1,31 +1,52 @@
 import React from "react";
-import { Button, FormGroup, ControlLabel, FormControl, Jumbotron } from "react-bootstrap";
+import {
+  Button,
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  Grid,
+  Row,
+  Col
+} from "react-bootstrap";
 
 const LoginLayout = ({ handleSubmit, state, handleInputChange }) => {
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <FormGroup controlId="loginGroup">
-          <ControlLabel>Username</ControlLabel>
-          <FormControl
-            type="text"
-            name="username"
-            value={state.login}
-            onChange={handleInputChange}
-          />
-        </FormGroup>
-        <FormGroup controlId="passwordGroup">
-          <ControlLabel>Password</ControlLabel>
-          <FormControl
-            type="password"
-            name="password"
-            value={state.password}
-            onChange={handleInputChange}
-          />
-        </FormGroup>
-        <Button type="submit">Default</Button>
-      </form>
-    </div>
+    <Grid>
+      <Row>
+        <Col
+          xs={6}
+          sm={6}
+          md={6}
+          lg={4}
+          xsOffset={3}
+          smOffset={3}
+          mdOffset={3}
+          lgOffset={4}
+        >
+          <form onSubmit={handleSubmit}>
+            <FormGroup controlId="loginGroup">
+              <ControlLabel>Username</ControlLabel>
+              <FormControl
+                type="text"
+                name="username"
+                value={state.login}
+                onChange={handleInputChange}
+              />
+            </FormGroup>
+            <FormGroup controlId="passwordGroup">
+              <ControlLabel>Password</ControlLabel>
+              <FormControl
+                type="password"
+                name="password"
+                value={state.password}
+                onChange={handleInputChange}
+              />
+            </FormGroup>
+            <Button type="submit">Default</Button>
+          </form>
+        </Col>
+      </Row>
+    </Grid>
   );
 };
 
