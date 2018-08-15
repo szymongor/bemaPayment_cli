64a3cadca697ff62685d220cedd1b1487f0f8d65
Karolina <karolina.chrzaszcz@euvic.pl>
2018-08-15 13:20:42 +0200

    refactor

--- src/components/login/login_layout.js
+++ src/components/login/login_layout.js
@@ -10,54 +10,62 @@ import {
   Col
 } from "react-bootstrap";
 
-function FieldGroup({ id, label, ...props }) {
+const FieldGroup = ({ id, label, ...props }) => {
   return (
     <FormGroup controlId={id}>
       <ControlLabel>{label}</ControlLabel>
       <FormControl {...props} />
     </FormGroup>
   );
-}
+};
 
-const LoginLayout = ({ handleSubmit, state, handleInputChange }) => {
+const GridWrapper = props => {
+  let colParams = {
+    xs: 10,
+    sm: 8,
+    md: 6,
+    lg: 4,
+    xsOffset: 1,
+    smOffset: 2,
+    mdOffset: 3,
+    lgOffset: 4
+  };
   return (
     <Grid className="login-form">
       <Row>
-        <Col
-          xs={10}
-          sm={8}
-          md={6}
-          lg={4}
-          xsOffset={1}
-          smOffset={2}
-          mdOffset={3}
-          lgOffset={4}
-          className="border-layout"
-        >
-          <form onSubmit={handleSubmit}>
-            <FieldGroup
-              id="loginGroup"
-              label="Username"
-              type="text"
-              name="username"
-              value={state.login}
-              onChange={handleInputChange}
-            />
-            <FieldGroup
-              id="passwordGroup"
-              label="Password"
-              type="password"
-              name="password"
-              value={state.password}
-              onChange={handleInputChange}
-            />
-            <Button bsStyle="primary" type="submit" className="pull-right">
-              Login
-            </Button>
-          </form>
+        <Col {...colParams} className="border-layout">
+          {props.children}
         </Col>
       </Row>
     </Grid>
+  );
+};
+
+const LoginLayout = ({ handleSubmit, state, handleInputChange }) => {
+  return (
+    <GridWrapper>
+      <form onSubmit={handleSubmit}>
+        <FieldGroup
+          id="loginGroup"
+          label="Username"
+          type="text"
+          name="username"
+          value={state.login}
+          onChange={handleInputChange}
+        />
+        <FieldGroup
+          id="passwordGroup"
+          label="Password"
+          type="password"
+          name="password"
+          value={state.password}
+          onChange={handleInputChange}
+        />
+        <Button bsStyle="primary" type="submit" className="pull-right">
+          Login
+        </Button>
+      </form>
+    </GridWrapper>
   );
 };
 
